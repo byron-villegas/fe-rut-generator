af73036c0a7abd804a90b3fc13c82fc8b7e7dd96
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-03-12 02:46:25 -0300

    Added express index to deploy

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+const PATH = '/'
+
+app.get(PATH, (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}${PATH}`);
+});
